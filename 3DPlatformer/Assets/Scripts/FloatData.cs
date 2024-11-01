d777de473dce5001f80d98512d92f5e210ac8b84
AJ Middleton <11019728@uvu.edu>
2024-11-01 13:01:29 -0600

    AI Behavior Started

--- 3DPlatformer/Assets/Scripts/FloatData.cs
+++ 3DPlatformer/Assets/Scripts/FloatData.cs
@@ -4,14 +4,37 @@ using UnityEngine;
 public class FloatData : ScriptableObject
 {
     public float value;
+    public float minValue;
+    public float maxValue;
     
     public void SetValue(float num)
     {
-        value = num;
+        if(num > maxValue)
+        {
+            value = maxValue;
+        }
+        else if(num < minValue)
+        {
+            value = minValue;
+        }
+        else
+        {
+            value = num;
+        }
+        
     }
     
     public void UpdateValue(float num)
     {
         value += num;
+        
+        if(value > maxValue)
+        {
+            value = maxValue;
+        }
+        else if(value < minValue)
+        {
+            value = minValue;
+        }
     }
 }
